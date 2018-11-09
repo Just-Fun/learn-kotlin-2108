26a3e64d67b6c2b1045ab01f4e81833091d9f1d7
Sergii Zagryvyi <serzhcello@gmail.com>
2018-11-09 18:43:10 +0200

    init

---
+++ src/main/java/com/serzh/LearnKotlin2108Application.java
@@ -0,0 +1,12 @@
+package com.serzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnKotlin2108Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnKotlin2108Application.class, args);
+    }
+}
---
+++ src/test/java/com/serzh/LearnKotlin2108ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.serzh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnKotlin2108ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
